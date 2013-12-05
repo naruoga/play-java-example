ffa7d6c11062a1260b486e64f24935a70f7ca690
Naruhiko Ogasawara <ogasawara.naruhiko@miraitsystems.jp>
2013-12-05 17:02:53 +0900

    work in progress about login feature

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,17 +2,54 @@ package controllers;
 
 import models.Project;
 import models.Task;
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
+import static play.data.Form.form;
+
 public class Application extends Controller {
 
+    @Security.Authenticated(Secured.class)
     public static Result index() {
         return ok(index.render(
                 Project.find.all(),
                 Task.find.all()
         ));
+    }
+
+    public static Result login() {
+        return ok(
+                login.render(form(Login.class))
+        );
+    }
+
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                    routes.Application.index()
+            );
+        }
+    }
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
     }
 }
